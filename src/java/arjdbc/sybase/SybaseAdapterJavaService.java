31e7538a574bc96d30a70f6d63db87d42bba671d
arkadiy kraportov <arkadiyk@gmail.com>
2011-01-13 13:46:12 +0900

    remove extra debug output

--- src/java/arjdbc/sybase/SybaseAdapterJavaService.java
+++ src/java/arjdbc/sybase/SybaseAdapterJavaService.java
@@ -1,8 +1,5 @@
 /*
  **** BEGIN LICENSE BLOCK *****
- * Copyright (c) 2006-2010 Nick Sieger <nick@nicksieger.com>
- * Copyright (c) 2006-2007 Ola Bini <ola.bini@gmail.com>
- * Copyright (c) 2008-2009 Thomas E Enebo <enebo@acm.org>
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
@@ -35,12 +32,9 @@ import java.io.IOException;
 
 public class SybaseAdapterJavaService implements BasicLibraryService {
     public boolean basicLoad(final Ruby runtime) throws IOException {
-        System.out.println("*****  BASIC LOAD: " + runtime);
 
         RubyClass jdbcConnection = RubyJdbcConnection.createJdbcConnectionClass(runtime);
         SybaseRubyJdbcConnection.createSybaseJdbcConnectionClass(runtime, jdbcConnection);
-
-        System.out.println("****1  BASIC LOAD: " + jdbcConnection);
 
         return true;
     }
--- src/java/arjdbc/sybase/SybaseRubyJdbcConnection.java
+++ src/java/arjdbc/sybase/SybaseRubyJdbcConnection.java
@@ -1,6 +1,5 @@
 /*
  **** BEGIN LICENSE BLOCK *****
- * Copyright (c) 2011 Arkadiy Kraportov <arkadiyk@gmail.com>
  *
  * Permission is hereby granted, free of charge, to any person obtaining
  * a copy of this software and associated documentation files (the
