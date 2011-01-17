80b05a2e9e61d0e18ab92772ec24d1c75a6e9683
arkadiy kraportov <arkadiyk@gmail.com>
2011-01-17 13:31:01 +0900

    1) better handle limit with no offset. 2) case insensitive DISTINCT

--- lib/arel/visitors/sybase_jtds.rb
+++ lib/arel/visitors/sybase_jtds.rb
@@ -5,7 +5,7 @@ module Arel
       def select_count? o
         sel = o.cores.length == 1 && o.cores.first
         projections = sel.projections.length == 1 && sel.projections
-        Arel::Nodes::Count === projections.first
+        Arel::Nodes::Count === (projections.respond_to?(:first) && projections.first)
       end
 
 
@@ -13,22 +13,21 @@ module Arel
         if o.offset || (o.limit && select_count?(o))
           o.offset.expr += 1 if o.offset
           sql = super  # if offset OR (limit & count) use the Java limit/offset/count parser
-        else
+        elsif o.limit
           limit  = o.limit.expr
           o.limit = nil
           sql = super
-
-          if limit
-            limit_and_no_offset sql, limit
-          end
+          limit_and_no_offset sql, limit
+        else
+          sql = super
         end
 
         sql
       end
 
       def limit_and_no_offset sql, limit
-        if sql =~ /DISTINCT /
-          sql.gsub!(/DISTINCT /, "DISTINCT TOP #{limit} ")
+        if sql =~ /DISTINCT /i
+          sql.gsub!(/DISTINCT /i, "DISTINCT TOP #{limit} ")
         else
           sql.gsub!(/SELECT /, "SELECT TOP #{limit} ")
         end
